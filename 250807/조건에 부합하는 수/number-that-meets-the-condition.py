afb8f671c09a194305e92f40994ba825ee438b4e
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-07 19:08:57 +0900

    Title: 조건에 부합하는 수, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250807/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -0,0 +1,10 @@
+A = int(input())
+
+for i in range(1, A+1):
+    if i % 2 == 0 and i % 4 != 0:
+        continue
+    elif (i // 8) % 2 == 0:
+        continue
+    elif i % 7 < 4:
+        continue
+    print(i, end = " ")
