8c515a82903b16c97c94cc201ff4ea9927af6414
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-08 18:12:17 +0900

    Title: 별 그리기, Time: 75ms, Memory: 19MB, Status: Passed - Codetree

---
+++ 250808/별 그리기/star-drawing.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+# Please write your code here.
+for i in range(n):
+    for j in range(n-i-1):
+        print(end=" ")
+
+    for j in range(2*i + 1):
+        print("*", end = "")
+    print()
+
+for i in range(n-1, 0, -1):
+    for j in range(n-i):
+        print(end = " ")
+    for j in range(2*i-1):
+        print("*", end="")
+    print()
+
+# for i in range()
