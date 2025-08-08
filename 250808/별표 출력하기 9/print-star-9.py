b646fa0e42a0261fdf576f185eeaa35fb319cd75
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-08 17:50:59 +0900

    Title: 별표 출력하기 9, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250808/별표 출력하기 9/print-star-9.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+for i in range(N):
+    for j in range(N - i - 1):
+        print(end = "  ")
+    
+    for j in range(i*2 + 1):
+        print("*", end = " ")
+    
+    print()
