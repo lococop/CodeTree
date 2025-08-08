4fb00d71d018db1047150aa80d51e5154466e28c
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-08 17:36:17 +0900

    Title: 별표 출력하기 4, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250808/별표 출력하기 4/print-star-4.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for i in range(N, 0, -1):
+    for j in range(i):
+        print("*", end = " ")
+    print()
+
+for i in range(1, N):
+    for j in range(i+1):
+        print("*", end = " ")
+    print()
