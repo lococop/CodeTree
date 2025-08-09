85145cb64fde3d9f9f25190dbb15032c83909810
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-09 15:51:36 +0900

    Title: 1-9 개수 세기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250809/1-9 개수 세기/count-one-to-nine.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(1, 10):
+    cnt = 0
+    for elem in arr:
+        if elem == i:
+            cnt += 1
+        
+    print(cnt)
