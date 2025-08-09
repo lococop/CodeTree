6afa49c089a954754b0f6de64473405aa29da804
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-09 16:06:20 +0900

    Title: 십의 자리 숫자, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250809/십의 자리 숫자/number-of-tens-digit.py
+++ 250809/십의 자리 숫자/number-of-tens-digit.py
@@ -1,8 +1,16 @@
 arr = list(map(int, input().split()))
 
+arr1 = []
+
+for i in arr:
+    if i == 0:
+        break
+    else:
+        arr1.append(i)
+
 for i in range(1,10):
     cnt = 0
-    for elem in arr:
+    for elem in arr1:
         if elem//10 == i:
             cnt += 1
     print(f"{i} - {cnt}")
