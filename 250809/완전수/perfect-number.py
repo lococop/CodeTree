323379cfad1b42d5a8b787b786941ba7b8758a51
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-09 10:20:30 +0900

    Title: 완전수, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250809/완전수/perfect-number.py
@@ -0,0 +1,14 @@
+start, end = map(int, input().split())
+
+cnt = 0
+# Please write your code here.
+for i in range(start, end+1):
+    sum = 0
+    for j in range(1, i):
+        if i % j == 0:
+            sum += j
+    
+    if i == sum:
+        cnt += 1
+
+print(cnt)
