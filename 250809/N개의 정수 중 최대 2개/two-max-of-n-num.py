fc45941a28293086f25dad6a6bc64ddc902a278f
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-09 17:17:28 +0900

    Title: N개의 정수 중 최대 2개, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250809/N개의 정수 중 최대 2개/two-max-of-n-num.py
+++ 250809/N개의 정수 중 최대 2개/two-max-of-n-num.py
@@ -12,5 +12,12 @@ for elem in a:
     if max_num2 < elem and elem != max_num1:
         max_num2 = elem
 
+cnt = 0
+for elem in a:
+    if elem == max_num1:
+        cnt += 1
+if cnt == 2:
+    max_num2 = max_num1
+    
 print(max_num1, max_num2)
         
