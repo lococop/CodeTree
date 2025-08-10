2f78b8ac835bad5e709c9cec94a75502d6f86d90
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-10 13:45:38 +0900

    Title: 특정 문자의 등장 횟수, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250810/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -0,0 +1,12 @@
+a = input()
+
+cnt1 = 0
+cnt2 = 0
+
+for i in range(len(a)-1):
+    if a[i]+a[i+1] == 'ee':
+        cnt1 += 1
+    elif a[i]+a[i+1] == 'eb':
+        cnt2 += 1
+
+print(cnt1, cnt2)
