f0057a4bb59d88c916cc0e6c8247d157eaa3a6f7
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-11 13:50:48 +0900

    Title: 코드네임, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250811/코드네임/code-name.py
@@ -0,0 +1,24 @@
+MAX_N = 5
+
+codenames = []
+scores = []
+for _ in range(MAX_N):
+    codename, score = input().split()
+    codenames.append(codename)
+    scores.append(int(score))
+
+# Please write your code here.
+
+class f:
+    def __init__(self, codename, score):
+        self.codename = codename
+        self.score = score
+
+f1 = None
+f1 = None
+f1 = None
+f1 = None
+
+for i in range(MAX_N):
+
+    f+str(i) = f(codenames[i], scores[i])
