fd35e390ffa3f764a7845fc4407dcba6362de8d0
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-11 13:57:59 +0900

    Title: 코드네임, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250811/코드네임/code-name.py
+++ 250811/코드네임/code-name.py
@@ -13,12 +13,23 @@ class f:
     def __init__(self, codename, score):
         self.codename = codename
         self.score = score
+a = [
+    f(codenames[0], scores[0]),
+    f(codenames[1], scores[1]),
+    f(codenames[2], scores[2]),
+    f(codenames[3], scores[3]),
+    f(codenames[4], scores[4]),
+]
 
-f1 = None
-f1 = None
-f1 = None
-f1 = None
+import sys
+min_n = sys.maxsize
+min_class = None
 
 for i in range(MAX_N):
+    if min_n > a[i].score:
+        min_n = a[i].score
+        min_class = a[i]
 
-    f+str(i) = f(codenames[i], scores[i])
+
+print(min_class.codename, min_class.score)
+
