5b0f6eee21d8c25691877465386012042d3b5f2f
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-11 17:08:45 +0900

    Title: 그 요일은, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250811/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,33 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+# Please write your code here.
+days = [0,31, 28, 31, 30, 31, 30, 31,31,30,31,30,31]
+a = 0
+for i in range(m1):
+    a += days[i]
+a += d1
+
+b = 0
+for i in range(m2):
+    b += days[i]
+b += d2
+
+c = (b-a)
+if A == 'Mon':
+    c = c
+elif A == 'Tue':
+    c += 1
+elif A == 'Wed':
+    c += 2
+elif A == 'Thu':
+    c += 3
+elif A == 'Fri':
+    c += 4
+elif A == 'Sat':
+    c += 5
+elif A == 'Sun':
+    c += 6
+
+print(c//7)
+
