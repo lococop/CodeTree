49c80562451343e32df884ff7ca5e08c7bfc7828
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-12 12:25:19 +0900

    Title: 선두를 지켜라, Time: 95ms, Memory: 32MB, Status: Passed - Codetree

--- 250812/선두를 지켜라/keep-the-lead.py
+++ 250812/선두를 지켜라/keep-the-lead.py
@@ -22,43 +22,56 @@ Size = 1000000
 arr1 = [0]*Size
 arr2 = [0]*Size
 
-sum1 = 0
-sum2 = 0
-for i in range(n):
-    sum1 += v[i]*t[i]
-
-for i in range(m):
-    sum2 += v2[i]*t2[i]
-         
-min_t = min(sum1, sum2)
 
 
 
 
-time1 = 0
+
 distance1 = 0
+time1 = 0
 for i in range(n):
-    for _ in range(t[i]):
-        time1 += 1
+    for j in range(time1+1, time1+t[i]+1):
+        
         distance1 += v[i]
-        arr1[time1] = distance1
+        arr1[j] = distance1
+    time1 += t[i]
 
-# B 이동 기록
-time2 = 0
+        
+
 distance2 = 0
+time2 = 0
 for i in range(m):
-    for _ in range(t2[i]):
-        time2 += 1
+    for j in range(time2+1, time2+t2[i]+1):
+        
         distance2 += v2[i]
-        arr2[time2] = distance2
-
+        arr2[j] = distance2
+    time2 += t2[i]
 
 
 
 cnt = 0
-for i in range(2, min_t+1):
-    if (arr1[i] > arr2[i] and arr1[i-1] <= arr2[i-1]) or (arr1[i] < arr2[i] and arr1[i-1] >= arr2[i-1]):
-        cnt += 1
+leader = 0
+if arr1[1] > arr2[1]:
+    leader = 1
+elif arr2[1] > arr1[1]:
+    leader = 2
+
+new_leader = 0
+for i in range(2, sum(t)+1):
+
+    
+    if (arr1[i] > arr2[i] and arr1[i-1] <= arr2[i-1]):
+        new_leader = 1
+        if leader != 0 and leader != new_leader:
+            cnt += 1
+        leader = 1
+
+    if (arr1[i] < arr2[i] and arr1[i-1] >= arr2[i-1]):
+
+        new_leader = 2
+        if leader != 0 and leader != new_leader:
+            cnt += 1
+        leader = 2
 
 print(cnt)
     
