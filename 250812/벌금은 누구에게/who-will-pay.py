0eca703b07d9a40419d5ee8cd7df21bbb9026a44
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-12 11:01:20 +0900

    Title: 벌금은 누구에게, Time: 74ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250812/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,17 @@
+N, M, K = map(int, input().split())
+student = [int(input()) for _ in range(M)]
+
+# Please write your code here.
+
+arr = [0]*N
+
+for i in range(M):
+
+    arr[student[i]-1] += 1
+
+    if arr[student[i]-1] >= 3:
+        print(student[i])
+        break
+    
+    if i == M-1:
+        print(-1)
