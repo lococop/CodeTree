a336e278946a4fb5b4f55aa10d02e47d24b887d8
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-12 14:49:16 +0900

    Title: 좌우로 움직이는 로봇, Time: 54ms, Memory: 31MB, Status: Wrong Answer - Codetree

---
+++ 250812/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,82 @@
+n, m = map(int, input().split())
+
+# Process robot A's movements
+t = []
+d = []
+for _ in range(n):
+    time, direction = input().split()
+    t.append(int(time))
+    d.append(direction)
+
+# Process robot B's movements
+t_b = []
+d_b = []
+for _ in range(m):
+    time, direction = input().split()
+    t_b.append(int(time))
+    d_b.append(direction)
+
+# Please write your code here.
+# 위치가 저장된 배열을 순회하면서 위치정보가 같아지는 순간의 횟수를 구한다.
+
+# 시간에 따른 위치정보 배열
+Size = 1000000
+A_Location = [0]*Size
+B_Location = [0]*Size
+
+LocA = 0
+time = 0
+for i in range(n):
+    if d[i] == 'R':
+        for j in range(time+1, time+t[i]+1):
+            LocA += 1
+            A_Location[j] = LocA
+        time += t[i]
+    elif d[i] == 'L':
+        for j in range(time+1, time+t[i]+1):
+            LocA -= 1
+            A_Location[j] = LocA
+        time += t[i]
+
+
+LocB = 0
+timeB = 0
+for i in range(m):
+    if d_b[i] == 'R':
+        for j in range(timeB+1, timeB+t_b[i]+1):
+            LocB += 1
+            B_Location[j] = LocB
+        timeB += t_b[i]
+    elif d_b[i] == 'L':
+        for j in range(timeB+1, timeB+t_b[i]+1):
+            LocB -= 1
+            B_Location[j] = LocB
+        timeB += t_b[i]
+
+MAX_T = max(sum(t), sum(t_b))   # 최대 움직이는 시간
+
+move_cnt = 0
+
+if sum(t) < sum(t_b):
+    j = 2
+    for i in range(2, MAX_T+1):
+        
+        
+        if A_Location[j] == B_Location[i] and A_Location[j-1] != B_Location[i-1]:
+            move_cnt += 1
+        if j < sum(t):
+            j += 1
+else:
+    j = 2
+    for i in range(2, MAX_T+1):
+        
+        
+        if A_Location[i] == B_Location[j] and A_Location[i-1] != B_Location[j-1]:
+            move_cnt += 1
+        if j < sum(t):
+            j += 1
+
+print(move_cnt)
+
+
+
