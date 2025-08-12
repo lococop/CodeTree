d10164949a44ce34744cf515bd40ab97b851a626
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-12 16:41:00 +0900

    Title: 문자에 따른 명령 2, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250812/문자에 따른 명령 2/text-based-commands2.py
+++ 250812/문자에 따른 명령 2/text-based-commands2.py
@@ -5,9 +5,9 @@ x, y = 0, 0
 
 dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
 
-if dirs[0] == 'L':
+if dirs[0] == 'L' and dirs[1] == 'F':
     x, y = x + dx[(3+3)%4], y + dy[(3+3)%4]
-elif dirs[0] == 'R':
+elif dirs[0] == 'R' and dirs[1] == 'F':
     x, y = x + dx[(3+1)%4], y + dy[(3+1)%4]
 
 print(x, y)
