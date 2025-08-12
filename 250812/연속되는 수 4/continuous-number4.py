82da00b9293dc63a173ee67d80ce1af8aeeeabd6
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-12 09:23:12 +0900

    Title: 연속되는 수 4, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250812/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+
+import sys
+Max_n = -sys.maxsize
+
+cnt = 0
+
+for i in range(n):
+    if i == 0 or arr[i] > arr[i-1]:
+        cnt += 1
+    if arr[i] < arr[i-1]:
+        cnt = 1
+
+    if Max_n < cnt:
+        Max_n = cnt
+print(Max_n)
