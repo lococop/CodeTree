b72e3a0a308fb15c757dd42167b977b1698a7373
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-13 23:02:20 +0900

    Title: 데이터센터의 온도 조정 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250813/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,23 @@
+N, C, G, H = map(int, input().split())
+ranges = [tuple(map(int, input().split())) for _ in range(N)]
+
+# Please write your code here.
+
+def work(tem, machine):
+    if tem < ranges[machine][0]:
+        return C
+    elif ranges[machine][0] <= tem <= ranges[machine][1]:
+        return G
+    elif ranges[machine][1] < tem:
+        return H
+
+import sys
+max_q = -sys.maxsize
+
+for tem in range(0, 31):
+    sum_q = 0
+    for machine in range(N):
+        sum_q += work(tem, machine)
+    max_q = max(max_q, sum_q)
+
+print(max_q)
