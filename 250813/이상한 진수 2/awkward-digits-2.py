9dee612ed175e53b5f7bf9c3433b8b16e6b352f6
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-13 10:41:10 +0900

    Title: 이상한 진수 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250813/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,21 @@
+a = input()
+
+# Please write your code here.
+
+Max_Num = ""
+
+for i in range(len(a)):
+    if a[i] == '0':
+        Max_Num += '1'
+        Max_Num += a[i+1:]
+        break
+    else:
+        Max_Num += a[i]
+
+ans = 0
+
+for i in range(len(Max_Num)):
+    ans = ans * 2 + int(Max_Num[i])
+
+print(ans)
+    
