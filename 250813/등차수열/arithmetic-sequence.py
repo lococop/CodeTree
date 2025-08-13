1ee0a5f6195a9a14a351a26eef599ccab44abb0c
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-13 23:32:45 +0900

    Title: 등차수열, Time: 58ms, Memory: 17MB, Status: Passed - Codetree

--- 250813/등차수열/arithmetic-sequence.py
+++ 250813/등차수열/arithmetic-sequence.py
@@ -3,13 +3,18 @@ a = list(map(int, input().split()))
 
 # Please write your code here.
 
-a.sort()
+import sys
 
-cnt = 0
-for i in range(n):
-    for j in range(i+1, n): 
-        for k in range(1, 101):
+max_n = -sys.maxsize
+
+for k in range(1, 101):
+    cnt = 0
+
+
+    for i in range(n):
+        for j in range(i+1, n):
             if a[j]-k == k-a[i]:
                 cnt += 1
+    max_n = max(max_n, cnt)
 
-print(cnt)
+print(max_n)
