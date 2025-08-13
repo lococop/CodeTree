596e2818e2b1d93970b1d08971245d7923c70477
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-13 09:27:58 +0900

    Title: 일렬로 서있는 소 2, Time: 56ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250813/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,19 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+
+ans = 0
+
+for i in range(N):
+
+    for j in range(i+1, N):
+
+        if i < j and A[i] <= A[j]:
+            for k in range(j+1, N):
+                if i < j < k and A[i] <= A[j] <= A[k]:
+                    ans += 1
+
+
+print(ans)
+
