2dcfd4e16cc16a8b6b448e0c62dff561f181acd2
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-13 22:24:03 +0900

    Title: 이상한 폭탄 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250813/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,17 @@
+N, K = map(int, input().split())
+num = [int(input()) for _ in range(N)]
+
+# Please write your code here.
+import sys
+
+max_n = -sys.maxsize
+
+for i in range(N-(K+1)+1):
+    for j in range(i, i+K+1):
+        if num[i:i+K+1].count(num[j]) >= 2:
+            max_n = max(max_n, num[j])
+
+if max_n == -sys.maxsize:
+    print(-1)
+else:
+    print(max_n)
