982df47d6145f4a3878a2f9b34a4f85d8ac6ad93
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-13 18:01:45 +0900

    Title: 개발자의 능력 3, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250813/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,20 @@
+abilities = list(map(int, input().split()))
+
+# Please write your code here.
+
+import sys
+min_diff = sys.maxsize
+
+def get_diff(i, j, k):
+    sum1 = abilities[i] + abilities[j] + abilities[k]
+    sum2 = sum(abilities) - sum1
+
+    return abs(sum1-sum2)
+
+for i in range(len(abilities)):
+    for j in range(i+1, len(abilities)):
+        for k in range(i+2, len(abilities)):
+
+            min_diff = min(min_diff, get_diff(i, j, k))
+
+print(min_diff)
