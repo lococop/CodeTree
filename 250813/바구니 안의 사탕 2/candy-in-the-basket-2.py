add52d5d1446dc6cdbe7a00cde0438ee867e7448
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-13 16:04:03 +0900

    Title: 바구니 안의 사탕 2, Time: 56ms, Memory: 16MB, Status: Passed - Codetree

--- 250813/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250813/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -19,10 +19,11 @@ for i in range(N):
 import sys
 max_candy = -sys.maxsize
 
-for i in range(0, size-K*2+1):
+for i in range(size):
     sum_candy = 0
-    for j in range(i, i+2*K+1):
-        sum_candy += arr[j]
+    for j in range(i-K, i+K+1):
+        if j >= 0 and j <= size:
+            sum_candy += arr[j]
     
     max_candy = max(max_candy, sum_candy)
 
