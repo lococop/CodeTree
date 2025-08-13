72489f214c9f9d3e335971bf9978fdef38e4d0ab
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-13 23:14:07 +0900

    Title: 등차수열, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250813/등차수열/arithmetic-sequence.py
@@ -0,0 +1,15 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+# Please write your code here.
+
+a.sort()
+
+cnt = 0
+for i in range(n):
+    for j in range(i+1, n): 
+        for k in range(a[i]+1, a[j]):
+            if a[j]-k == k-a[i]:
+                cnt += 1
+
+print(cnt)
