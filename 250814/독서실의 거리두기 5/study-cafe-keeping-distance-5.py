5b3384452e6ed7d2432836e4f7f6e38d5b30b22f
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-14 11:04:40 +0900

    Title: 독서실의 거리두기 5, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250814/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 250814/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,41 +1,30 @@
-import sys
+n = int(input())
+seat = list(input())
 
-N = int(input())
-seat = input()
 
-max_d = -sys.maxsize
+def min_dist():
+    dist = n
+    # 둘 다 1인 곳에 대해
+    # 모든 쌍을 조사하여, 그 중 가장 가까운 거리를 구합니다.
+    for i in range(n):
+        for j in range(i + 1, n):
+            if seat[i] == '1' and seat[j] == '1':
+                dist = min(dist, j - i)
+    
+    return dist
 
-for person in range(N):
-    if seat[person] == '1':  # 이미 사람 있음
-        continue
 
-    temp = list(map(int, seat))
-    temp[person] = 1
+ans = 0
+# 들어갈 위치를 일일이 정해보며
+# 그 상황에서 가장 가까운 사람간의 거리를 구해
+# 가능한 경우 중 최댓값을 계산합니다.
+for i in range(n):
+    if seat[i] == '0':
+        # 비어있는 위치에 인원을 배치합니다.
+        seat[i] = '1'
+        # 가장 가까운 사람간의 거리를 구해 최댓값을 갱신해줍니다.
+        ans = max(ans, min_dist())
+        # 다시 채워졌던 값을 되돌려줍니다.
+        seat[i] = '0'
 
-    # 왼쪽 가장 가까운 사람
-    l_point = -1
-    for left in range(person - 1, -1, -1):
-        if temp[left] == 1:
-            l_point = left
-            break
-
-    # 오른쪽 가장 가까운 사람
-    r_point = -1
-    for right in range(person + 1, N):
-        if temp[right] == 1:
-            r_point = right
-            break
-
-    # 거리 계산
-    if l_point == -1 and r_point == -1:
-        continue  # 사람 한 명도 없는 경우
-    elif l_point == -1:
-        min_d = r_point - person
-    elif r_point == -1:
-        min_d = person - l_point
-    else:
-        min_d = min(person - l_point, r_point - person)
-
-    max_d = max(max_d, min_d)
-
-print(0 if max_d == -sys.maxsize else max_d)
+print(ans)
