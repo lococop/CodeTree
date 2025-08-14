d766e4344c0fc88fd8e65a6b0c05b7eb93fe92b8
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-14 10:55:27 +0900

    Title: 독서실의 거리두기 5, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250814/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 250814/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,49 +1,38 @@
+import sys
+
 N = int(input())
 seat = input()
 
-# Please write your code here.
-# N번 반복하면서 0인 자리에 인원을 추가한다.
-# 반복문을 두 번 사용하면서 최근에 추가한 인원 왼쪽 오른쪽에 있는 사람과의 거리가 최소가 되는 값을 찾는다.
-# 최소가 되는 값의 최댓값을 찾는 문제
-
-
-import sys
-
 max_d = -sys.maxsize
 
-
 for person in range(N):
-
-    if seat[person] == '1':  # 이미 사람이 있는 자리
+    if seat[person] == '1':  # 이미 사람 있음
         continue
 
     temp = list(map(int, seat))
     temp[person] = 1
-    
-    l_point = 0
-    r_point = 0
 
-    # 추가한 인원 기준 왼쪽에 있는 사람의 위치를 찾는다.
-    for left in range(person-1, -1, -1):
+    # 불가능한 값으로 초기화
+    l_point = -sys.maxsize
+    r_point = sys.maxsize
 
+    # 왼쪽 가장 가까운 사람
+    for left in range(person - 1, -1, -1):
         if temp[left] == 1:
             l_point = left
             break
-    
-    # 추가한 인원 기준 오른쪽에 있는 사람의 위치를 찾는다.
-    for right in range(person+1, N):
 
+    # 오른쪽 가장 가까운 사람
+    for right in range(person + 1, N):
         if temp[right] == 1:
             r_point = right
             break
 
-    
-
-    min_d = min(abs(person-l_point), abs(person-r_point))
+    # 거리 계산 (한쪽이 없으면 그쪽 거리는 무한대라서 min에서 제외)
+    dist_left = abs(person - l_point) if l_point != -sys.maxsize else sys.maxsize
+    dist_right = abs(person - r_point) if r_point != sys.maxsize else sys.maxsize
+    min_d = min(dist_left, dist_right)
 
     max_d = max(max_d, min_d)
 
-if max_d == -sys.maxsize:
-    print(0)
-else:
-    print(max_d)
+print(0 if max_d == -sys.maxsize else max_d)
