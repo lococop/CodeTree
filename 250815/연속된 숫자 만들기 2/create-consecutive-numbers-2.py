79a1c36466bda2c3c51b2b343be02be621959bd7
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-15 12:34:31 +0900

    Title: 연속된 숫자 만들기 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250815/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 250815/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -5,7 +5,7 @@ pos = list(map(int, input().split()))
 if abs(pos[0] - pos[1]) == 1 and abs(pos[1]-pos[2]) == 1:
     print(0)
 
-elif (abs(pos[0] - pos[1]) == 1 and abs(pos[1]-pos[2]) != 1) or abs(pos[0] - pos[1]) != 1 and abs(pos[1]-pos[2]) == 1:
+elif abs(pos[0]-pos[1]) == 2 or abs(pos[1]-pos[2]) == 2:
     print(1)
 else:
     print(2)
