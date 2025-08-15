b6980227267b5aba4820f2618da99fcc1a391f22
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-15 10:31:56 +0900

    Title: 구역 청소, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250815/구역 청소/district-cleaning.py
@@ -0,0 +1,9 @@
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+
+# Please write your code here.
+if b < c or d < a:
+    print((b-a)+(d-c))
+
+else:
+    print(max(b,d)-min(a,c))
