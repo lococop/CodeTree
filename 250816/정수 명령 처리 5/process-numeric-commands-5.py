75ffd2f363007ccd65e3eb3348f1b0ae04f861d3
gwan woo Ryu <58384645+lococop@users.noreply.github.com>
2025-08-16 11:19:34 +0900

    Title: 정수 명령 처리 5, Time: 121ms, Memory: 21MB, Status: Passed - Codetree

---
+++ 250816/정수 명령 처리 5/process-numeric-commands-5.py
@@ -0,0 +1,26 @@
+N = int(input())
+
+command = []
+num = []
+
+for _ in range(N):
+    line = input().split()
+    command.append(line[0])
+    if line[0] == "push_back" or line[0] == "get":
+        num.append(int(line[1]))
+    else:
+        num.append(0)
+
+# Please write your code here.
+
+arr = []
+
+for i in range(N):
+    if command[i] == "push_back":
+        arr.append(num[i])
+    elif command[i] == "get":
+        print(arr[num[i]-1])
+    elif command[i] == "size":
+        print(len(arr)) 
+    elif command[i] == "pop_back":
+        arr.pop(-1)
